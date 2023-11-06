06d6dd9da6c30ae80c91f6ee80ca759b2f4decd9
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-11-06 10:48:45 -0300

    chore: installing gorilla/mux

--- routes/routes.go
+++ routes/routes.go
@@ -5,10 +5,14 @@ import (
 	"net/http"
 
 	"github.com/alessandro-maciel/go-rest-api/controllers"
+	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalities", controllers.PersonalityIndex)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalities", controllers.PersonalityIndex)
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
